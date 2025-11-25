a06b3afd5ed43c384d92d4e7bad6a96a89a6cf92
King-khid <kingkhid60@gmail.com>
2025-11-25 18:39:04 -0100

    fix: bug fix

--- src/app/dashboard/components/Topbar.tsx
+++ src/app/dashboard/components/Topbar.tsx
@@ -75,7 +75,6 @@ export default function Topbar() {
                         </h2>
                         <p className="text-sm text-center text-gray-500">User Profile</p>
 
-                        {/* Info Section */}
                         <div className="mt-6 space-y-4">
                             <div className="flex items-center justify-center gap-3">
                                 <Mail className="w-5 h-5 text-blue-500" />
